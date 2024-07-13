2f168f169d633dcbab07441378a239e977d3de8d
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-07-13 12:24:58 +0530

    Footer

--- nike-landing-page/src/components/Footer.jsx
+++ nike-landing-page/src/components/Footer.jsx
@@ -4,7 +4,10 @@ function Footer() {
   return (
     <div>
         <main>
-            Footer 
+            <h3>Company T&C</h3>
+            <li>Open For EMp</li>
+            <li>Detail</li>
+            <li>No-Refund</li>
         </main>
     </div>
   )
