e5fa24cbf2e224fc67a30c554149f67f6c2e7721
Virendra1807 <108993954+Virendra1807@users.noreply.github.com>
2024-08-11 17:35:43 +0530

    All About Props in react

--- daily_practice/src/App.js
+++ daily_practice/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import PropsUse from './Components/PropsUse';
+// import PropsUse from './Components/PropsUse';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PropsUse heading="Props in React" />
+        {/* <PropsUse heading="Props in React"  age={23}/> */}
+        
+
       </header>
     </div>
   );
--- daily_practice/src/Components/PropsUse.jsx
+++ daily_practice/src/Components/PropsUse.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import PropTypes from "prop-types"
 
-function PropsUse(props) {
-  //props.heading = "Props Can't change";
+
+function PropsUse(props) {    //{name, age} PROPS Destructuring
+  // We can not change props bcoz props are immutable
+  //props.heading = "Props Can't change";`
+
+
+PropsUse.propTypes = {
+  name : PropTypes.string,
+  age: PropTypes.number
+}
+
   return (
-    <div>{props.heading}</div>
+    <div>
+      <h1>{props.heading}</h1>
+      <h3>My name is {props.name} and i'm {props.age} years old.</h3>
+      <p><small>'Jayesh' is default prop and 'Age' is passed from App component</small></p>
+    </div>
+
   )
 }
+
+
+  // Default PRops in react
+  PropsUse.defaultProps={
+    name : 'Jayesh',
+    age : 27
+  }
+  
+
 
 export default PropsUse
